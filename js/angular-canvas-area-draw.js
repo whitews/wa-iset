a3cdc2eb678779bb4f2581fbfc5085bb8abb21e5
Scott White <whitews@gmail.com>
2017-04-14 10:10:32 -0400

    clean up dotLineLength

--- js/angular-canvas-area-draw.js
+++ js/angular-canvas-area-draw.js
@@ -38,23 +38,49 @@ polyDraw.directive('ngPolyDraw', [function () {
         },
         controller: function () {
             this.dotLineLength = function(x, y, x0, y0, x1, y1, o) {
-                function lineLength(x, y, x0, y0){
+
+                function lineLength(x, y, x0, y0) {
                     return Math.sqrt((x -= x0) * x + (y -= y0) * y);
                 }
-                if(o && !(o = function(x, y, x0, y0, x1, y1){
-                        if(!(x1 - x0)) return {x: x0, y: y};
-                        else if(!(y1 - y0)) return {x: x, y: y0};
-                        var left, tg = -1 / ((y1 - y0) / (x1 - x0));
-                        return {x: left = (x1 * (x * tg - y + y0) + x0 * (x * - tg + y - y1)) / (tg * (x1 - x0) + y0 - y1), y: tg * left - tg * x + y};
-                    }(x, y, x0, y0, x1, y1), o.x >= Math.min(x0, x1) && o.x <= Math.max(x0, x1) && o.y >= Math.min(y0, y1) && o.y <= Math.max(y0, y1))){
-                    var l1 = lineLength(x, y, x0, y0), l2 = lineLength(x, y, x1, y1);
-                    return l1 > l2 ? l2 : l1;
+
+                function find_line_endpoints(x, y, x0, y0, x1, y1) {
+                    if (!(x1 - x0)) {
+                        return {x: x0, y: y};
+                    } else if (!(y1 - y0)) {
+                        return {x: x, y: y0};
+                    }
+
+                    var left, tg = -1 / ((y1 - y0) / (x1 - x0));
+
+                    return {
+                        x: left = (x1 * (x * tg - y + y0) + x0 * (x * - tg + y - y1)) / (tg * (x1 - x0) + y0 - y1),
+                        y: tg * left - tg * x + y
+                    };
                 }
-                else {
-                    var a = y0 - y1, b = x1 - x0, c = x0 * y1 - y0 * x1;
-                    return Math.abs(a * x + b * y + c) / Math.sqrt(a * a + b * b);
+
+                var alt_o = find_line_endpoints(x, y, x0, y0, x1, y1);
+                var min_x = Math.min(x0, x1);
+                var max_x = Math.max(x0, x1);
+                var min_y = Math.min(y0, y1);
+                var max_y = Math.max(y0, y1);
+
+                if (o && !(o = alt_o)) {
+                    if (o.x >= min_x && o.x <= max_x && o.y >= min_y && o.y <= max_y) {
+
+                        var l1 = lineLength(x, y, x0, y0);
+                        var l2 = lineLength(x, y, x1, y1);
+
+                        return l1 > l2 ? l2 : l1;
+                    }
                 }
+
+                var a = y0 - y1,
+                    b = x1 - x0,
+                    c = x0 * y1 - y0 * x1;
+
+                return Math.abs(a * x + b * y + c) / Math.sqrt(a * a + b * b);
             };
+
             this.hexToRgb = function(hex){
                 var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
                 return result ? {
